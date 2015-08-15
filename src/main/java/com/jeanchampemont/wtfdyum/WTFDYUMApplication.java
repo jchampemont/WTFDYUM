2b5c946e62d20db35e313cad02d9e1d157a40ab9
Jean Champémont <git@jeanchampemont.com>
2015-08-15 13:56:02 +0200

    Add Spring Boot skeleton :skull:

---
+++ src/main/java/com/jeanchampemont/wtfdyum/WTFDYUMApplication.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2015 WTFDYUM
+ *
+ * This file is part of the WTFDYUM project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jeanchampemont.wtfdyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Application Class
+ *
+ * @author Jean Champémont
+ *
+ */
+@SpringBootApplication
+public class WTFDYUMApplication {
+
+    /**
+     * Main
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(WTFDYUMApplication.class, args);
+    }
+}

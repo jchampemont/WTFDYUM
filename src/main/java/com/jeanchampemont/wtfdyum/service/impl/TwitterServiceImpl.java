e725d00be2ce062e959830d1b64da053e44e0321
Jean Champémont <jchampemont@users.noreply.github.com>
2016-03-29 11:02:04 +0200

    #12 improve rate limit handling
    - update cursor...
    - check rate limit status only when needed

--- src/main/java/com/jeanchampemont/wtfdyum/service/impl/TwitterServiceImpl.java
+++ src/main/java/com/jeanchampemont/wtfdyum/service/impl/TwitterServiceImpl.java
@@ -88,9 +88,11 @@ public class TwitterServiceImpl implements TwitterService {
             final long cursor = -1;
             do {
                 followersIDs = twitter.getFollowersIDs(userId, cursor);
-
-                checkRateLimitStatus(followersIDs.getRateLimitStatus(),
+                if(followersIDs.hasNext()) {
+                    cursor = followersIDs.getNextCursor();
+                    checkRateLimitStatus(followersIDs.getRateLimitStatus(),
                         WTFDYUMExceptionType.GET_FOLLOWERS_RATE_LIMIT_EXCEEDED);
+                }
 
                 final Set<Long> currentFollowers = Arrays.stream(followersIDs.getIDs()).boxed()
                         .collect(Collectors.toCollection(() -> new HashSet<>()));
